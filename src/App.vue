ed071c186da9ef45f04f8ca9436d7a7d438d7b07
nicola <sieben.nicola@gmail.com>
2023-03-21 18:09:59 +0100

    sorting option in progress

--- src/App.vue
+++ src/App.vue
@@ -23,44 +23,173 @@
       <v-divider class="my-4"></v-divider>
 
       <v-row>
-        <v-col v-for="(task, index) in taskList" :key="index" cols="12" sm="6" md="4">
-          <v-card class="ma-2 rounded-shaped" :style="{ backgroundColor: task.color }">
-            <v-card-title>
-              <!-- Task title -->
-              <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
-              <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
-              <!-- Task description -->
-                <v-list-item class="text-subtitle-1" v-if="!task.editable">{{ task.description }}</v-list-item>
-                <v-text-field v-model="task.description" v-else label="Task Description"></v-text-field>
-            </v-card-title>
-    
+        
+        <v-col cols="4" class="bg-purple-lighten-4">
+          <v-col-title>To do</v-col-title>
+        <v-col v-for="(task, index) in taskList" :key="index">
+            <div v-if="task.status === 'to do'" >
+                <v-card class="ma-2 rounded-shaped" :style="{ backgroundColor: task.color }">
+                <v-card-title>
+                  <!-- Task title -->
+                  <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
+                  <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
+                  <!-- Task title -->
 
+                  <!-- Task description -->
+                  <v-list-item class="text-subtitle-1" v-if="!task.editable">{{ task.description }}</v-list-item>
+                  <v-text-field v-model="task.description" v-else label="Task Description"></v-text-field>
+                  <!-- Task description -->
 
-            <v-card-actions class="justify-end text-right">
-              <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
-              <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
-              <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
-              <!-- Color changer in a dialog component -->
-              <v-dialog v-model="task.colorPickerDialog" width="auto">
-                <template v-slot:activator="{ props }">
-                  <v-card-actions color="primary px-0" v-bind="props" @click="task.colorPickerDialog = true">
-                    <v-icon color="orange-darken-2 " size="25">mdi-palette</v-icon>
-                  </v-card-actions>
-                </template>
+                  <v-radio-group v-model="task.status">
+                    <v-radio label="To Do" value="to do"></v-radio>
+                    <v-radio label="Progress" value="progress"></v-radio>
+                    <v-radio label="Done" value="done"></v-radio>
+                  </v-radio-group>
 
-                <v-card>
-                  <v-card-text>
-                    <v-color-picker v-model="task.color"></v-color-picker>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn color="primary" block @click="task.colorPickerDialog = false, updateTask(index)">Save changes</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
+                </v-card-title>
 
-            </v-card-actions>
-          </v-card>
+                <v-card-actions class="justify-end text-right">
+                  <!-- Actions -->
+                  <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
+                  <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
+                  <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
+                  <!-- Actions -->
 
+                  <!-- Color changer dialog component -->
+                  <v-dialog v-model="task.colorPickerDialog" width="auto">
+                    <template v-slot:activator="{ props }">
+                      <v-card-actions color="primary px-0" v-bind="props" @click="task.colorPickerDialog = true">
+                        <v-icon color="orange-darken-2 " size="25">mdi-palette</v-icon>
+                      </v-card-actions>
+                    </template>
+                    <v-card>
+                      <v-card-text>
+                        <v-color-picker v-model="task.color"></v-color-picker>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn color="primary" block @click="task.colorPickerDialog = false, updateTask(index)">Save
+                          changes</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-card-actions>
+                <!-- Color changer dialog component -->
+
+              </v-card>
+            </div>
+          </v-col>
+        </v-col>
+
+        <v-col cols="4" class="bg-orange-lighten-4">
+          <v-col-title>progress</v-col-title>
+          <v-col v-for="(task, index) in taskList" :key="index">
+            <div v-if="task.status === 'progress'" >
+                <v-card class="ma-2 rounded-shaped" :style="{ backgroundColor: task.color }">
+                <v-card-title>
+                  <!-- Task title -->
+                  <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
+                  <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
+                  <!-- Task title -->
+
+                  <!-- Task description -->
+                  <v-list-item class="text-subtitle-1" v-if="!task.editable">{{ task.description }}</v-list-item>
+                  <v-text-field v-model="task.description" v-else label="Task Description"></v-text-field>
+                  <!-- Task description -->
+
+                  <v-radio-group v-model="task.status">
+                    <v-radio label="To Do" value="to do"></v-radio>
+                    <v-radio label="Progress" value="progress"></v-radio>
+                    <v-radio label="Done" value="done"></v-radio>
+                  </v-radio-group>
+
+                </v-card-title>
+
+                <v-card-actions class="justify-end text-right">
+                  <!-- Actions -->
+                  <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
+                  <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
+                  <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
+                  <!-- Actions -->
+
+                  <!-- Color changer dialog component -->
+                  <v-dialog v-model="task.colorPickerDialog" width="auto">
+                    <template v-slot:activator="{ props }">
+                      <v-card-actions color="primary px-0" v-bind="props" @click="task.colorPickerDialog = true">
+                        <v-icon color="orange-darken-2 " size="25">mdi-palette</v-icon>
+                      </v-card-actions>
+                    </template>
+                    <v-card>
+                      <v-card-text>
+                        <v-color-picker v-model="task.color"></v-color-picker>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn color="primary" block @click="task.colorPickerDialog = false, updateTask(index)">Save
+                          changes</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-card-actions>
+                <!-- Color changer dialog component -->
+
+              </v-card>
+            </div>
+          </v-col>
+        </v-col>
+
+        <v-col cols="4" class="bg-green-lighten-4">
+          <v-col-title>Done</v-col-title>
+          <v-col v-for="(task, index) in taskList" :key="index">
+            <div v-if="task.status === 'done'" >
+                <v-card class="ma-2 rounded-shaped" :style="{ backgroundColor: task.color }">
+                <v-card-title>
+                  <!-- Task title -->
+                  <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
+                  <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
+                  <!-- Task title -->
+
+                  <!-- Task description -->
+                  <v-list-item class="text-subtitle-1" v-if="!task.editable">{{ task.description }}</v-list-item>
+                  <v-text-field v-model="task.description" v-else label="Task Description"></v-text-field>
+                  <!-- Task description -->
+
+                  <v-radio-group v-model="task.status">
+                    <v-radio label="To Do" value="to do"></v-radio>
+                    <v-radio label="Progress" value="progress"></v-radio>
+                    <v-radio label="Done" value="done"></v-radio>
+                  </v-radio-group>
+
+                </v-card-title>
+
+                <v-card-actions class="justify-end text-right">
+                  <!-- Actions -->
+                  <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
+                  <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
+                  <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
+                  <!-- Actions -->
+
+                  <!-- Color changer dialog component -->
+                  <v-dialog v-model="task.colorPickerDialog" width="auto">
+                    <template v-slot:activator="{ props }">
+                      <v-card-actions color="primary px-0" v-bind="props" @click="task.colorPickerDialog = true">
+                        <v-icon color="orange-darken-2 " size="25">mdi-palette</v-icon>
+                      </v-card-actions>
+                    </template>
+                    <v-card>
+                      <v-card-text>
+                        <v-color-picker v-model="task.color"></v-color-picker>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn color="primary" block @click="task.colorPickerDialog = false, updateTask(index)">Save
+                          changes</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-card-actions>
+                <!-- Color changer dialog component -->
+
+              </v-card>
+            </div>
+          </v-col>
         </v-col>
       </v-row>
 
@@ -76,7 +205,7 @@ export default {
       newTask: '',
       newTaskDescription: '',
       tasks: [],
-      dialog: false,
+      dialog: false
     };
   },
   methods: {
@@ -93,7 +222,13 @@ export default {
     // If task is empty, return alert with message "Please write a task"
     addTask() {
       if (this.newTask != "") {
-        this.taskList.push({ title: this.newTask,description: this.newTaskDescription, done: false, color: "#ffffff", colorPickerDialog: false });
+        this.taskList.push({ title: this.newTask,
+                              description: this.newTaskDescription,
+                              done: false,
+                              color: "#ffffff",
+                              colorPickerDialog: false,
+                              status: "to do" // status propriety 
+                            });
         this.newTask = '';
         this.newTaskDescription = '';
         this.saveData(this.taskList);
@@ -113,6 +248,7 @@ export default {
     // Uptdating task
     updateTask(index) {
       this.taskList[index].editable = false;
+      // this.taskList[index].status = this.taskList[index].status;
       this.saveData(this.taskList);
     },
     toggleTask(index) {
