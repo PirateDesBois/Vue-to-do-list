38b6a5cb5dec47e54698245d9a2d8ecc92e727fe
nicola <sieben.nicola@gmail.com>
2023-03-21 15:58:19 +0100

    save color in LocalStorage after closing dialog component

--- src/App.vue
+++ src/App.vue
@@ -7,64 +7,57 @@
     </v-app-bar>
 
     <v-container>
-  <v-card>
-    <v-card-title>
-      Add Task
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field v-model="newTask" label="Task Name"></v-text-field>
-        <v-btn color="primary" @click="addTask">Add</v-btn>
-      </v-form>
-    </v-card-text>
-  </v-card>
-
-  <v-divider class="my-4"></v-divider>
-
-  <v-row>
-    <v-col v-for="(task, index) in taskList" :key="index" cols="12" sm="6" md="4">
-      <v-card class="ma-2 rounded-shaped" :style="{backgroundColor: task.color}">
-  <v-card-title>
-    <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
-    <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
-  </v-card-title>
-  <v-card-actions class="justify-space-between text-right">
-    <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
-    <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
-    <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
-    <!-- <v-color-picker v-model="task.color"></v-color-picker> -->
-    <v-dialog
-  v-model="task.colorPickerDialog"
-  width="auto"
->
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="primary"
-          v-bind="props"
-          @click="task.colorPickerDialog = true"
-        >
-          Change color
-        </v-btn>
-      </template>
-
       <v-card>
+        <v-card-title>
+          Add Task
+        </v-card-title>
         <v-card-text>
-          <v-color-picker v-model="task.color"></v-color-picker>
+          <v-form>
+            <v-text-field v-model="newTask" label="Task Name"></v-text-field>
+            <v-btn color="primary" @click="addTask">Add</v-btn>
+          </v-form>
         </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" block @click="dialog = false">Close color changer</v-btn>
-        </v-card-actions>
       </v-card>
-    </v-dialog>
-    
-  </v-card-actions>
-</v-card>
 
-</v-col>
+      <v-divider class="my-4"></v-divider>
 
-  </v-row>
+      <v-row>
+        <v-col v-for="(task, index) in taskList" :key="index" cols="12" sm="6" md="4">
+          <v-card class="ma-2 rounded-shaped" :style="{ backgroundColor: task.color }">
+            <v-card-title>
+              <v-list-item v-if="!task.editable">{{ task.title }}</v-list-item>
+              <v-text-field v-model="task.title" v-else label="Task Name"></v-text-field>
+            </v-card-title>
+            <v-card-actions class="justify-end text-right">
+              <v-icon color="red" @click="deleteTask(index)">mdi-delete</v-icon>
+              <v-icon color="primary" v-if="!task.editable" @click="editTask(index)">mdi-pencil</v-icon>
+              <v-icon color="primary" v-else @click="updateTask(index)">mdi-check</v-icon>
+              <!-- Color changer in a dialog component -->
+              <v-dialog v-model="task.colorPickerDialog" width="auto">
+                <template v-slot:activator="{ props }">
+                  <v-card-actions color="primary px-0" v-bind="props" @click="task.colorPickerDialog = true">
+                    <v-icon color="orange-darken-2 " size="25">mdi-palette</v-icon>
+                  </v-card-actions>
+                </template>
 
-</v-container>
+                <v-card>
+                  <v-card-text>
+                    <v-color-picker v-model="task.color"></v-color-picker>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn color="primary" block @click="task.colorPickerDialog = false, updateTask(index)">Save changes</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+
+            </v-card-actions>
+          </v-card>
+
+        </v-col>
+
+      </v-row>
+
+    </v-container>
   </v-app>
 </template>
 
@@ -91,14 +84,14 @@ export default {
     // Adding tasks to the list
     // If task is empty, return alert with message "Please write a task"
     addTask() {
-  if (this.newTask != "") {
-    this.taskList.push({ title: this.newTask, done: false, color: "#ffffff",colorPickerDialog: false  });
-    this.newTask = '';
-    this.saveData(this.taskList);
-  } else {
-    alert('Please write a task')
-  }
-},
+      if (this.newTask != "") {
+        this.taskList.push({ title: this.newTask, done: false, color: "#ffffff", colorPickerDialog: false });
+        this.newTask = '';
+        this.saveData(this.taskList);
+      } else {
+        alert('Please write a task')
+      }
+    },
     // Deleting tasks from the list and from de LocalStorage
     deleteTask(index) {
       this.taskList.splice(index, 1);
@@ -110,9 +103,9 @@ export default {
     },
     // Uptdating task
     updateTask(index) {
-  this.taskList[index].editable = false;
-  this.saveData(this.taskList);
-},
+      this.taskList[index].editable = false;
+      this.saveData(this.taskList);
+    },
     toggleTask(index) {
       this.taskList[index].done = !this.taskList[index].done;
       this.saveData(this.taskList);
