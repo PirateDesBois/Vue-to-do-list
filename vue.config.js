ac3e16799d7b244ef0a82fa6e3d026b8006314ad
nicola <sieben.nicola@gmail.com>
2023-03-22 11:56:35 +0100

    deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/',
 
   pluginOptions: {
     vuetify: {
